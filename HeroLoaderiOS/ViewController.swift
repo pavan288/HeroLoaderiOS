5a04ed5117c76e80a94c7e58f6f5edc7fb9a2e70
Pavan Powani <pavan.powani@ectosense.com>
2022-04-11 19:08:48 +0530

    Uncomment HeroLoader dependent code in ViewController

--- HeroLoaderiOS/ViewController.swift
+++ HeroLoaderiOS/ViewController.swift
@@ -5,50 +5,50 @@
 //  Created by Pavan Powani on 11/04/22.
 //
 
-//import HeroLoader
+import HeroLoader
 import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
-//    var viewModel: ViewModel?
-//
-//    var presentableHeros = [PresentableHeroStat]()
+    var viewModel: ViewModel?
+
+    var presentableHeros = [PresentableHeroStat]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        setupTableView()
-//        viewModel = ViewModel(api: URLSessionAPI(), onHeroLoad: refreshHeroTable)
-//        viewModel?.getHeroStats()
+        setupTableView()
+        viewModel = ViewModel(api: URLSessionAPI(), onHeroLoad: refreshHeroTable)
+        viewModel?.getHeroStats()
     }
 
-//    func refreshHeroTable(_ heroes: [HeroStatsAPI]?) -> Void {
-//        for hero in heroes ?? [] {
-//            self.presentableHeros.append(hero.asPresentableHeroStat())
-//            DispatchQueue.main.async {
-//                self.tableView.reloadData()
-//            }
-//        }
-//
-//    }
-//
-//    func setupTableView() {
-//        tableView.dataSource = self
-//        tableView.delegate = self
-//    }
-//}
-//
-//extension ViewController: UITableViewDelegate, UITableViewDataSource {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return presentableHeros.count
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cellData = presentableHeros[indexPath.row]
-//        let cell = UITableViewCell()
-//        cell.imageView?.image = UIImage(data: cellData.imageData)
-//        cell.textLabel?.text = cellData.name
-//        return cell
-//    }
+    func refreshHeroTable(_ heroes: [HeroStatsAPI]?) -> Void {
+        for hero in heroes ?? [] {
+            self.presentableHeros.append(hero.asPresentableHeroStat())
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+
+    }
+
+    func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return presentableHeros.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellData = presentableHeros[indexPath.row]
+        let cell = UITableViewCell()
+        cell.imageView?.image = UIImage(data: cellData.imageData)
+        cell.textLabel?.text = cellData.name
+        return cell
+    }
 }
 
 
